123f67369a4f9d8f7542dd610a9dde9a644f1832
Ockham-Byron <46241522+Ockham-Byron@users.noreply.github.com>
2024-03-13 14:27:26 +0100

    Update settings.py

--- tasty_moments/core/settings.py
+++ tasty_moments/core/settings.py
@@ -98,13 +98,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': env('DATABASE_NAME'),
-    'USER': env('DATABASE_USER'),
-    'PASSWORD': env('DATABASE_PASS'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
